f4cd27acf05f15b10e4985e00e28ffae0e06c247
undrown.off@gmail.com <undrown.off@gmail.com>
2019-12-22 12:41:20 +0200

    fix: fixed bug "100kop" (remastered method)

--- app/src/main/java/com/undrown/nosmoking/MoneySavedFormat.kt
+++ app/src/main/java/com/undrown/nosmoking/MoneySavedFormat.kt
@@ -1,5 +1,6 @@
 package com.undrown.nosmoking
 
+import kotlin.math.floor
 import kotlin.math.roundToInt
 
 class MoneySavedFormat(timeStart:Long, timeCur:Long) {
@@ -9,10 +10,17 @@ class MoneySavedFormat(timeStart:Long, timeCur:Long) {
     private val rubSavedPerSec = rubSavedPerDay/secInDay            //экономия в секунду
     private val rubSaved = rubSavedPerSec * secDelta
     private val usdSaved = (rubSaved/16.5f)
-    private val rub = if(rubSaved - rubSaved.roundToInt() > 0)rubSaved.roundToInt() else rubSaved.roundToInt() - 1
-    private val kop = ((rubSaved - rub)*100 - 0.5).roundToInt()
-    private val usd = if(usdSaved - usdSaved.roundToInt() > 0)usdSaved.roundToInt() else usdSaved.roundToInt() - 1
-    private val cent = ((usdSaved - usd)*100 - 0.5).roundToInt()
+    //right way:
+    private val rub = floor(rubSaved)
+    private val kop = ((rubSaved - rub)*100).toInt()
+    private val usd = floor(usdSaved)
+    private val cent = ((usdSaved - usd)*100).toInt()
+    //wrong way:
+    //private val rub = if(rubSaved - rubSaved.roundToInt() > 0)rubSaved.roundToInt() else rubSaved.roundToInt() - 1
+    //private val kop = ((rubSaved - rub)*100).roundToInt()
+    //private val usd = if(usdSaved - usdSaved.roundToInt() > 0)usdSaved.roundToInt() else usdSaved.roundToInt() - 1
+    //private val cent = ((usdSaved - usd)*100).roundToInt()
+
     val vRub = "$rub руб, $kop к."
     val vUSD = "$usd $, $cent c."
 }
